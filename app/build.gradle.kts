32d1101d24a50fc5e98afe37113cee41dcabfbd4
shamerdanov <shamil332211@list.ru>
2024-05-23 15:57:58 +0400

    add coroutines dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -65,9 +65,8 @@ dependencies {
 
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
-
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.3.9")
     implementation("com.github.Spikeysanju:MotionToast:1.4")
-
     implementation("com.github.bumptech.glide:glide:4.16.0")
 
     testImplementation(libs.junit)
